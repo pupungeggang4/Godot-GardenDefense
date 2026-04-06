ceacad7cea14a8f31c672a9d397d1cf1b4e9d7ce
pupungeggang6 <pupungeggang6@gmail.com>
2026-04-07 00:07:18 +0900

    Scene transition

--- game/script/scene/character_select.gd
+++ game/script/scene/character_select.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+
+func _on_button_back_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton and event.pressed:
+        if event.button_index == MOUSE_BUTTON_LEFT:
+            get_tree().change_scene_to_file('res://scene/title.tscn')
--- game/script/scene/title.gd
+++ game/script/scene/title.gd
@@ -1,1 +1,22 @@
 extends Node2D
+
+func _ready() -> void:
+    pass
+    
+func _process(delta: float) -> void:
+    pass
+
+func _on_button_start_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton and event.pressed:
+        if event.button_index == MOUSE_BUTTON_LEFT:
+            get_tree().change_scene_to_file('res://scene/character_select.tscn')
+
+func _on_button_collection_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton and event.pressed:
+        if event.button_index == MOUSE_BUTTON_LEFT:
+            get_tree().change_scene_to_file('res://scene/collection.tscn')
+
+func _on_button_quit_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton and event.pressed:
+        if event.button_index == MOUSE_BUTTON_LEFT:
+            get_tree().quit()
